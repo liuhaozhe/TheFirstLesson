ee10159b586ba3cb132e43b44148943559967957
HZ <liuhaozhe@outlook.com>
2020-05-18 13:39:15 +0800

    最后还是用字符转倒叙数0做的

--- cntZeroOfFac.py
+++ cntZeroOfFac.py
@@ -1,24 +1,26 @@
+# import math
+# n = eval(input("Please enter the number to be calculated:"))#接收数据
+# cnt = 1
+# fac = math.factorial(n) #计算阶乘
+# while fac>0: #循环和一个其实没有实际用处的循环条件
+#     fac = (fac / 10) #每次循环都把阶乘除以10（相当于抹掉小数点后一1位）
+#     # if float(fac/10)!=int(fac/10):#当除以10，有小数点和不能有小数点两种结果不等时，跳出循环
+#     #     break
+#     print(fac)
+#     if fac%10==0:#假如阶乘能被10整除（除10）没有余数，计数器加1
+#         cnt = cnt +1
+#     else:
+#         break
+# print("{}!={},output: {}".format(n,math.factorial(n),cnt))
+
+
 import math
 n = eval(input("Please enter the number to be calculated:"))
-cnt = 0
 fac = math.factorial(n)
-while fac>0:
-    cnt = cnt+1
-    fac = float(fac/10)
-    print(float(fac/10))
-    if float(fac/10)!=int(fac/10):
-        break
+cnt = 0
+for a in str(fac)[::-1]:
+  if eval(a)==0:
+    cnt+=1
+  else:
+    break
 print("{}!={},output: {}".format(n,math.factorial(n),cnt))
-
-
-# code from Zhao Jiaqi
-# import math as m
-# s=eval(input("Please input a positive integer: "))
-# fact = m.factorial(s)
-# count=0
-# for a in str(fact)[::-1]:
-#     if eval(a)==0:
-#         count+=1
-#     else :
-#         break
-# print("{}!={})".format(s,fact),",Output:",count)
